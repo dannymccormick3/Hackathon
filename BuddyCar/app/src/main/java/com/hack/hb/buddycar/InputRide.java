d19365e30f43721a94f712719a39f09b4bf8d70b
rileymr1 <michaelriley811@gmail.com>
2016-11-12 13:22:44 -0600

    michael's 1st commit

--- BuddyCar/app/src/main/java/com/hack/hb/buddycar/InputRide.java
+++ BuddyCar/app/src/main/java/com/hack/hb/buddycar/InputRide.java
@@ -1,7 +1,9 @@
 package com.hack.hb.buddycar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class InputRide extends AppCompatActivity {
@@ -12,4 +14,15 @@ public class InputRide extends AppCompatActivity {
         setContentView(R.layout.activity_input_ride);
         final Button b = (Button) findViewById(R.id.button8);
     }
+
+
+    //go to BuddySelect activity.
+    public void goBuddySelect (View view) {
+        Intent intent = new Intent(this, BuddySelection.class);
+        startActivity(intent);
+    }
+
+
+
+
 }
