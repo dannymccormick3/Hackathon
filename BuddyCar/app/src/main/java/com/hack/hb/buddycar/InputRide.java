ab0c7920e6cbd0b9bf3a7ec2aeb0d951c805ac5f
rileymr1 <michaelriley811@gmail.com>
2016-11-12 14:00:23 -0600

    Mike's push 2

--- BuddyCar/app/src/main/java/com/hack/hb/buddycar/InputRide.java
+++ BuddyCar/app/src/main/java/com/hack/hb/buddycar/InputRide.java
@@ -1,28 +1,102 @@
 package com.hack.hb.buddycar;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class InputRide extends AppCompatActivity {
+
+    public final static String START_CITY = "com.hack.hb.buddycar.startCity";
+    public final static String END_CITY = "com.hack.hb.buddycar.endCity";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_ride);
         final Button b = (Button) findViewById(R.id.button8);
+        final Button c = (Button) findViewById(R.id.button9);
+
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InputRide.this, BuddySelection.class));
+            }
+        });
+
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InputRide.this, BuddySelection.class));
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
     //go to BuddySelect activity.
-    public void goBuddySelect (View view) {
+    public void goBuddySelect(View view) {
         Intent intent = new Intent(this, BuddySelection.class);
+        EditText startCityText = (EditText) findViewById(R.id.editText8);
+        EditText endCityText = (EditText) findViewById(R.id.editText9);
+        String startCityStr = startCityText.getText().toString();
+        String endCityStr = endCityText.getText().toString();
+        intent.putExtra(START_CITY, startCityStr);
+        intent.putExtra(END_CITY, endCityStr);
         startActivity(intent);
     }
 
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("InputRide Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
