4938824edf9b9aa00d41b61b2fb253c5f537384c
rileymr1 <michaelriley811@gmail.com>
2016-11-12 11:56:32 -0600

    TEXT_HERE

---
+++ BuddyCar/app/src/main/java/com/hack/hb/buddycar/InputRide.java
@@ -0,0 +1,15 @@
+package com.hack.hb.buddycar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class InputRide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_ride);
+        final Button b = (Button) findViewById(R.id.button8);
+    }
+}
