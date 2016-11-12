5bab2a2be14151b91978b79926d505ebf234a7b4
Daniel McCormick <daniel.mccormick@vanderbilt.edu>
2016-11-12 12:50:31 -0600

    not much

---
+++ BuddyCar/app/src/main/java/com/hack/hb/buddycar/Database.java
@@ -0,0 +1,12 @@
+package com.hack.hb.buddycar;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+import com.amazonaws.services.dynamodbv2.model.*;
+/**
+ * Created by Danny on 11/12/2016.
+ */
+public class Database {
+
+}
